03f6a45c9f7bef869ce23bbfbd2fe84cb36af0ae
Murdo Maclachlan <murdomaclachlan@gmail.com>
2020-08-24 18:24:46 +0100

    Create main.py

---
+++ cdremover/main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+import praw
+import time
+import datetime
+from config import blacklist, cutoff, os, user
+
+version = "0.1"
+
+def getDate(comment):
+	return comment.created_utc
+
+while True:
+    reddit = praw.Reddit("credentials", user_agent=os+":claimdoneremover:v"+version+" (by u/MurdoMaclachlan)")
+
+    for comment in reddit.redditor(user).stream.comments():
+        if comment.body in blacklist:
+            if time.time()-getDate(comment)>cutoff*3600:
+                print("Past its sell-by date; deleting.")
+                comment.delete()
+    print("Connection lost. Reconnecting...")
