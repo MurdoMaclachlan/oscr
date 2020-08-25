54db73fd3b710003ffd8b506d9d12a19f0e8fd6b
Murdo Maclachlan <murdomaclachlan@gmail.com>
2020-08-25 09:57:08 +0100

    Added "unclaim" to blacklist

--- cdremover/config.py
+++ cdremover/config.py
@@ -1,4 +1,4 @@
 user = "" # Your username.
 os = "" # Your operating system.
-blacklist = ["claim", "done"] # The exact, word-for-word body of the comments to be deleted.
+blacklist = ["claim", "done", "unclaim"] # The exact, word-for-word body of the comments to be deleted.
 cutoff = 3 # The number of hours old which comments must be before they are deleted.
