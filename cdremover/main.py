d0858deb3ca842aeaeac8ca9702373c7d1d54c7a
Murdo Maclachlan <murdomaclachlan@gmail.com>
2020-08-25 18:39:15 +0100

    Update main.py

--- cdremover/main.py
+++ cdremover/main.py
@@ -2,7 +2,7 @@
 import praw
 import time
 import libcdr
-from config import blacklist, cutoff, limit, os, unit, user, wait
+from config import blacklist, cutoff, limit, os, unit, updateLog, user, wait
 
 version = "0.2 dev"
 
@@ -13,16 +13,25 @@ def getDate(comment):
 
 reddit = praw.Reddit("credentials", user_agent=os+":claimdoneremover:v"+version+" (by u/MurdoMaclachlan)")
 
-totalCounted = libcdr.fetch("counted")
-totalDeleted = libcdr.fetch("deleted")
+log = []
+
+totalCounted = libcdr.fetch("counted", log)
+totalDeleted = libcdr.fetch("deleted", log)
+
+if updateLog == True:
+	libcdr.doLog("Updating log...", log)
+	libcdr.writeLog(log)
 
 while True:
+	log = []
 	deleted = 0
 	counted = 0
 	nonCutoff = 0
+
 	for comment in reddit.redditor(user).comments.new(limit=limit):
 		if comment.body in blacklist:
 			if time.time() - getDate(comment) > cutoff*3600:
+				libcdr.doLog("Obsolete '{}' found, deleting.".format(comment.body), log)
 				comment.delete()
 				deleted += 1
 			else:
@@ -31,12 +40,17 @@ while True:
 
 	totalCounted += counted
 	totalDeleted += deleted
-	libcdr.update("counted", totalCounted)
-	libcdr.update("deleted", totalDeleted)
-	print("Totals:\nCounted: {}\nDeleted: {}\n".format(str(totalCounted),str(totalDeleted)))
-	print("This Run:\nCounted: {}\nDeleted: {}\nWaiting For: {}\n".format(str(counted),str(deleted),str(nonCutoff)))
+	libcdr.update("counted", totalCounted, log)
+	libcdr.update("deleted", totalDeleted, log)
+	libcdr.doLog("Totals:\nCounted: {}\nDeleted: {}\n".format(str(totalCounted),str(totalDeleted)), log)
+	libcdr.doLog("This Run:\nCounted: {}\nDeleted: {}\nWaiting For: {}\n".format(str(counted),str(deleted),str(nonCutoff)), log)
 	if wait == 1:
-		print("Waiting 1 {} before checking again...\n\n---").format(unit[0])
+		libcdr.doLog(("Waiting 1 {} before checking again...\n\n---").format(unit[0]), log)
 	else:
-		print("Waiting {} {} before checking again...\n\n---".format(str(wait), unit[1]))
+		libcdr.doLog("Waiting {} {} before checking again...\n\n---".format(str(wait), unit[1]), log)
+
+	if updateLog == True:
+		libcdr.doLog("Updating log...\n---", log)
+		libcdr.writeLog(log)
+
 	time.sleep(wait*unit[2])
