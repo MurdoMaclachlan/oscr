bf180c384dcc94a74c5927872b50a3a21c781dfc
Murdo Maclachlan <murdomaclachlan@gmail.com>
2020-08-25 18:35:36 +0100

    Update config.py

--- cdremover/config.py
+++ cdremover/config.py
@@ -5,3 +5,4 @@ cutoff = 3 # The number of hours old which comments must be before they are dele
 limit = 100 # How many comments to check through in the user's history, max 1000 (enter None).
 wait = 10 # How many units of time the program should wait before checking for new comments.
 unit = ["minute", "minutes", 60] # The unit of time used for the wait configuration. The number should be the unit converted into seconds.
+updateLog = True # Whether or not the log.txt file should be updated. Change to "False" if you don't want new logs.
