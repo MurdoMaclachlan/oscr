55084dc793e64dbec1df2a166c4cee4fc14ae674
Murdo Maclachlan <murdomaclachlan@gmail.com>
2020-08-25 16:08:32 +0100

    Added limit, wait and unit configurations
    Defaults initially set to 100, 10 and minutes.

--- cdremover/config.py
+++ cdremover/config.py
@@ -2,3 +2,6 @@ user = "" # Your username.
 os = "" # Your operating system.
 blacklist = ["claim", "done", "unclaim"] # The exact, word-for-word body of the comments to be deleted.
 cutoff = 3 # The number of hours old which comments must be before they are deleted.
+limit = 100 # How many comments to check through in the user's history, max 1000 (enter None).
+wait = 10 # How many units of time the program should wait before checking for new comments.
+unit = ["minute", "minutes", 60] # The unit of time used for the wait configuration. The number should be the unit converted into seconds.
