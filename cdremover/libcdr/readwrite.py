edbe26dc3ee9abac29910036d200a50d32a98647
Murdo Maclachlan <murdomaclachlan@gmail.com>
2020-08-26 14:06:02 +0100

    Update readwrite.py

--- cdremover/libcdr/readwrite.py
+++ cdremover/libcdr/readwrite.py
@@ -2,16 +2,35 @@ from .misc import doLog
 
 def fetch(statistic, log):
 
-    file = open("data/stats.txt", "r")
+    try:    
+        file = open("data/stats.txt", "r")
+    except FileNotFoundError:
+        doLog("No stats for {} found; returning 0.".format(statistic), log)
+        return 0
+    
     content = file.read().splitlines()
     result = []
 
-    # I swear this shithole of a for-if-for-if tree must be condensable
+    # If it can't find any statistics
+    if content == []:
+        doLog("No stats for {} found; returning 0.".format(statistic), log)
+        return 0
+    
+    # If it can find the right statistic (I swear this shithole of a for-if-for-if tree must be condensable)
     for line in content:
         if list(line)[0] == list(statistic)[0]:
+            found = True
             for i in list(line):
                 if i.isnumeric():
                     result.append(str(i))
+            break
+        else:
+            found = False
+
+    # If it can only find one statistic, and it isn't the right one
+    if found == False:
+        doLog("No stats for {} found; returning 0.".format(statistic), log)
+        return 0
 
     file.close()
     doLog("Fetched {} successfully.".format(statistic), log)
@@ -19,22 +38,44 @@ def fetch(statistic, log):
 
 def update(statistic, value, log):
 
+    content = []    
     lineToReplace = 0
     newLine = "{}: {}".format(statistic, str(value))
 
-    with open("data/stats.txt", "r") as file:
-        content = file.read().splitlines()
+    try:
+        with open("data/stats.txt", "r") as file:
+            content = file.read().splitlines()
+    except FileNotFoundError:
+        doLog("No stats.txt file found; creating.", log)
 
     with open("data/stats.txt", "w") as file:
+        
+        # If it can't find any statistics
         if content == []:
-            file.write(newLine)
+            if statistic == "counted":
+                file.write(newLine+"\ndeleted: 0")
+            else:
+                file.write("counted: 0\n"+newLine)
             doLog("Updated {} successfully.".format(statistic), log)
             return True
 
+        # If it can find the right statistic
         for line in content:
             if list(line)[0] == list(statistic)[0]:
+                found = True
                 lineToReplace = content.index(line)
-
+                break
+            else:
+                found = False
+        
+        # If it can only find one statistic, and it isn't the right one
+        if found == False:
+            content.append(newLine)
+            file.seek(0)
+            file.write('\n'.join(content))
+            doLog("Updated {} successfully.".format(statistic), log)
+            return True
+                
         content[lineToReplace] = newLine
 
         file.write('\n'.join(content))
