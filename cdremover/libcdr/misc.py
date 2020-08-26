7c6cf33c52e1fb88049bee4919363f8ec85660bf
Murdo Maclachlan <murdomaclachlan@gmail.com>
2020-08-26 17:17:18 +0100

    Update misc.py

--- cdremover/libcdr/misc.py
+++ cdremover/libcdr/misc.py
@@ -1,3 +1,6 @@
+import os
+from os.path import isfile, isdir
+
 def doLog(output, log):
     log.append(output+"\n")
     print(output)
@@ -8,3 +11,14 @@ def writeLog(log):
         for i in log:
             file.write(i)
     return True
+    
+def attemptLog(log):
+    try:
+        writeLog(log)
+    except FileNotFoundError:
+        doLog("No log.txt found; creating.", log)
+        if not isdir("data"):
+            os.mkdir("data")
+            writeLog(log)
+        if not isfile("data/logs.txt"):
+            writeLog(log)
