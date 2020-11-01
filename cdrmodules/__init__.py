b005ec9bad26efdac91c407b96a3e8d8e62b4df2
Murdo Maclachlan <murdomaclachlan@gmail.com>
2020-11-01 16:57:44 +0000

    Restructure + add createIni() import

--- cdrmodules/__init__.py
+++ cdrmodules/__init__.py
@@ -1,9 +1,12 @@
 # miscellaneous functions
 from .misc import getConfig
+from .misc import createIni
 from .misc import getTime
+from .misc import getDate
 
 # log handling functions functions
 from .log import doLog
+from .log import updateLog
 from .log import writeLog
 from .log import attemptLog
 
