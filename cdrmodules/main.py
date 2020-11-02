53b53c41b89dbd239f03d0271c6fecb70914d5ac
Murdo Maclachlan <murdomaclachlan@gmail.com>
2020-11-02 18:30:22 +0000

    Update main.py

--- cdrmodules/main.py
+++ cdrmodules/main.py
@@ -7,48 +7,42 @@ import praw
 import time
 import sys
 import configparser
-from os.path import expanduser
 from .__init__ import *
+from .gvars import *
 
-home = expanduser("~")
-
-config = getConfig(home)
-
-version = "1.0.0rc4"
-
-instance = False
-attempts = 0
-while instance == False:
-    if attempts == 3:
-        print("Could not find praw.ini file after 3 attempts. Exiting.")
-        sys.exit(0)
-    try:
-        reddit = praw.Reddit("credentials", user_agent=config["os"]+":claimdoneremover:v"+version+" (by u/MurdoMaclachlan)")
-        instance = True
-    except configparser.NoSectionError:
-        createIni(home)
-        attempts += 1
-
-log = []
-doLog("Running CDRemover version {} with recur set to {}.".format(version, config["recur"]), log)
+# config setup and start message
+config = getConfig()
+doLog("Running CDRemover version {} with recur set to {}.".format(version, config["recur"]))
 
 # Retrieves stats
-totalCounted = fetch("counted", log, home)
-totalDeleted = fetch("deleted", log, home)
+totalCounted = fetch("counted")
+totalDeleted = fetch("deleted")
+
+# Initialises Reddit() instance
+try:
+    reddit = praw.Reddit("cdrcredentials", user_agent=config["os"]+":claimdoneremover:v"+version+" (by u/MurdoMaclachlan)")
+except (configparser.NoSectionError, praw.exceptions.MissingRequiredAttributeException):
+    if createIni():
+        doLog("praw.ini successfully created, program restart required for this to take effect.")
+        updateLog("Exiting...", config)
+    else:
+        doLog("Failed to create praw.ini file, something went wrong.")
+        updateLog("Exiting...", config)
+    sys.exit(0)
 
 def remover(comment, cutoff, deleted, waitingFor):
-    if time.time() - getDate(comment) > cutoff*3600:
-        doLog("Obsolete '{}' found, deleting.".format(comment.body), log)
+    if time.time() - getDate(comment) > cutoff:
+        doLog("Obsolete '{}' found, deleting.".format(comment.body))
         comment.delete()
         deleted += 1
     else:
-        doLog("Waiting for '{}'.".format(comment.body), log)
+        doLog("Waiting for '{}'.".format(comment.body))
         waitingFor += 1
     return deleted, waitingFor
 
 if config["logUpdates"] == True:
-    logUpdates, log = updateLog("Updating log...", log, config, home)
-    logUpdates, log = updateLog("Log updated successfully.", log, config, home)
+    logUpdates = updateLog("Updating log...", config)
+    logUpdates = updateLog("Log updated successfully.", config)
 
 while True:
     deleted = 0
@@ -57,41 +51,43 @@ while True:
 
     # Checks all the user's comments, deleting them if they're past the cutoff.
     for comment in reddit.redditor(config["user"]).comments.new(limit=config["limit"]):
-        if config["torOnly"] == True:
+        if config["torOnly"]:
             if comment.body.lower() in config["blacklist"] and str(comment.subreddit).lower() == "transcribersofreddit":
-                deleted, waitingFor = remover(comment, config["cutoff"], deleted, waitingFor)
+                deleted, waitingFor = remover(comment, config["cutoffSec"], deleted, waitingFor)
         else:
             if comment.body.lower() in config["blacklist"]:
-                deleted, waitingFor = remover(comment, config["cutoff"], deleted, waitingFor)
+                deleted, waitingFor = remover(comment, config["cutoffSec"], deleted, waitingFor)
         counted += 1
+        if counted % 25 == 0:
+            doLog("{}/{} comments checked successfuly.".format(counted, config["limit"]))
 
     # Updates statistics
     totalCounted += counted
     totalDeleted += deleted
-    update("counted", totalCounted, log, home)
-    update("deleted", totalDeleted, log, home)
+    update("counted", totalCounted)
+    update("deleted", totalDeleted)
     
     # Gives info about this iteration; how many comments were counted, deleted, still waiting for.
-    doLog("Counted this cycle: {}".format(str(counted)), log)
-    doLog("Deleted this cycle: {}".format(str(deleted)), log)
-    doLog("Waiting for: {}".format(str(waitingFor)), log)
-    doLog("Total Counted: {}".format(str(totalCounted)), log)
-    doLog("Total Deleted: {}".format(str(totalDeleted)), log)
+    doLog("Counted this cycle: {}".format(str(counted)))
+    doLog("Deleted this cycle: {}".format(str(deleted)))
+    doLog("Waiting for: {}".format(str(waitingFor)))
+    doLog("Total Counted: {}".format(str(totalCounted)))
+    doLog("Total Deleted: {}".format(str(totalDeleted)))
 
     # If recur is set to false, updates log and kills the program.
-    if config["recur"] == False:
-        logUpdates, log = updateLog("Updating log...", log, config, home)
-        logUpdates, log = updateLog("Log updated successfully.", log, config, home)
-        updateLog("Exiting...", log, config, home)
+    if not config["recur"]:
+        logUpdates = updateLog("Updating log...", config)
+        logUpdates = updateLog("Log updated successfully.", config)
+        updateLog("Exiting...", config)
         break
 
     # Updates log, prepares for next cycle.
-    if logUpdates == True:
-        logUpdates, log = updateLog("Updating log...", log, config, home)
-        logUpdates, log = updateLog("Log updated successfully.", log, config, home)
-        doLog("Waiting {} {} before checking again...".format(str(config["wait"]), config["unit"][0] if config["wait"] == 1 else config["unit"][1]), log)
-        logUpdates, log = updateLog("", log, config, home)
+    if logUpdates:
+        logUpdates = updateLog("Updating log...", config)
+        logUpdates = updateLog("Log updated successfully.", config)
+        doLog("Waiting {} {} before checking again...".format(str(config["wait"]), config["unit"][0] if config["wait"] == 1 else config["unit"][1]))
+        logUpdates = updateLog("", config)
     else:
-        doLog("Waiting {} {} before checking again...".format(str(config["wait"]), config["unit"][0] if config["wait"] == 1 else config["unit"][1]), log)
+        doLog("Waiting {} {} before checking again...".format(str(config["wait"]), config["unit"][0] if config["wait"] == 1 else config["unit"][1]))
 
-    time.sleep(config["wait"]*config["unit"][2])
+    time.sleep(config["waitTime"])
