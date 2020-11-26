f7baa95ad6be2ee3a53bb3f3d84f405442fd03db
Murdo Maclachlan <murdomaclachlan@gmail.com>
2020-11-26 15:37:59 +0000

    Update misc.py
    - Change global variables to gvars class
    - Remove unnecessary imports

--- cdrmodules/misc.py
+++ cdrmodules/misc.py
@@ -1,32 +1,29 @@
 import datetime
-import time
 import json
 import sys
-from .gvars import *
 from os import environ, mkdir
-from os.path import isfile, isdir
+from os.path import isdir
 
 # Finds the current time and returns it in a human readable format.
 def getTime(timeToFind):
     currentTime = datetime.datetime.fromtimestamp(timeToFind)
     return currentTime.strftime("%Y-%m-%d %H:%M:%S")
 
-from .log import doLog, writeLog
+from .log import doLog
 
 # Retrieves the user configurations from a .json file, or creates a config file from default values if one can't be found.
-def getConfig():
-    
-    global home, log
+def getConfig(gvars):
 
     try:
-        with open(home+"/.cdremover/config.json") as configFile:
+        with open(gvars.home+"/.cdremover/config.json") as configFile:
             try:
                 fromConfig = json.load(configFile)
             except json.decoder.JSONDecodeError:
-                doLog("Failed to get config; could not decode JSON file. Exiting.")
+                doLog("Failed to get config; could not decode JSON file. Exiting.", gvars)
                 sys.exit(0)
-            config = fromConfig["config"][0]
-   
+            gvars.config = fromConfig["config"][0]
+            gvars = calculateEssentials(gvars)
+
     except FileNotFoundError:
         user = input("No config file found. Please enter your Reddit username:  /u/")
         os = input("Optionally, you may also enter your operating system. This is only used in the user_agent and can be left blank by simply pressing enter:  ")
@@ -58,38 +55,44 @@ def getConfig():
         outConfig = {}
         outConfig["config"] = []
         outConfig["config"].append(defaultConfig)
-        
-        try:
-            dumpConfig(outConfig)
-        except FileNotFoundError:
-            doLog("home/.cdremover directory not found; creating.")
-            if not isdir(home+"/.cdremover"):
-                mkdir(home+"/.cdremover")
-                dumpConfig(outConfig)
-            
-        config = outConfig["config"][0]
-        
-    # Performs any necessary one-time calculations and changes relating to the config
-    if config["limit"] >= 1000 or not str(config["limit"]).isnumeric():
-        config["limit"] = None
-    try:
-        config["cutoffSec"] = config["cutoff"]*config["cutoffUnit"]
-    except (KeyError, TypeError):
-        config["cutoffSec"] = config["cutoff"]*3600
-    config["waitTime"] = config["wait"]*config["unit"][2]
 
-    return config
+        try:
+            dumpConfig(outConfig, gvars)
+        except FileNotFoundError:
+            doLog("home/.cdremover directory not found; creating.", gvars)
+            if not isdir(gvars.home+"/.cdremover"):
+                mkdir(gvars.home+"/.cdremover")
+                dumpConfig(outConfig, gvars)
+            
+        gvars.config = outConfig["config"][0]
+
+    # Performs any necessary one-time calculations and changes relating to the config
+    gvars = calculateEssentials(gvars)
+    
+    print(gvars.config)
+    return gvars.config
+
+def calculateEssentials(gvars):
+    
+    if gvars.config["limit"] >= 1000 or not str(gvars.config["limit"]).isnumeric():
+        gvars.config["limit"] = None
+    try:
+        gvars.config["cutoffSec"] = gvars.config["cutoff"]*gvars.config["cutoffUnit"]
+    except (KeyError, TypeError):
+        gvars.config["cutoffSec"] = gvars.config["cutoff"]*3600
+    gvars.config["waitTime"] = gvars.config["wait"]*gvars.config["unit"][2]
+    
+    return gvars
 
 # Attempts to update the config file
-def dumpConfig(outConfig):
-    with open(home+"/.cdremover/config.json", "w") as outFile:
+def dumpConfig(outConfig, gvars):
+    
+    with open(gvars.home+"/.cdremover/config.json", "w") as outFile:
         outFile.write(json.dumps(outConfig, indent=4, sort_keys=True))
     return True
 
 # Creates praw.ini file, if it is missing
-def createIni():
-    
-    global home
+def createIni(gvars):
 
     platformConfs = {
         "linux": ".config",
@@ -98,8 +101,8 @@ def createIni():
     if sys.platform.startswith("win"):
         savePath = environ["APPDATA"]
     else:
-        savePath = home + platformConfs[sys.platform]
-    print("praw.ini incomplete or incorrect. It will need to be created.")
+        savePath = gvars.home + platformConfs[sys.platform]
+    doLog("praw.ini incomplete or incorrect. It will need to be created.", gvars)
     iniVars = {
         "client_id": input("Please input your client id:  "),
         "client_secret": input("Please input your client secret:  "),
