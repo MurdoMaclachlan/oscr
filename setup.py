65d54f881bb8243eb9cd3a43d6ba0e0f3f58674c
Murdo Maclachlan <murdo@maclachlans.org.uk>
2021-02-19 12:48:48 +0000

    just so pip show works better tbh

--- setup.py
+++ setup.py
@@ -24,5 +24,6 @@ setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
         "Operating System :: OS Independent",
-    ]
+    ],
+    license='GPLv3+'
 )
