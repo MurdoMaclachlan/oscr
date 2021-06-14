cd064838881d841a16a645304d9859d7703d80dc
Murdo Maclachlan <murdo@maclachlans.org.uk>
2021-06-14 21:38:55 +0100

    Refactor to OOP, bump version number

--- core/globals.py
+++ core/globals.py
@@ -17,12 +17,14 @@
     Contact me at murdo@maclachlans.org.uk
 """
 
-from os.path import expanduser
+from datetime import datetime
+from os import environ, mkdir
+from os.path import expanduser, isdir
 from sys import platform
 from colored import fg, attr
-from typing import NoReturn
-from .misc import defineSavePath
-global defaultConfig, VERSION
+from time import time
+from typing import Any, Dict, List, NoReturn, Union
+global defaultConfig, Globals, Log, Stats, System, VERSION
 
 defaultConfig = {
     "blacklist": [
@@ -64,26 +66,90 @@ defaultConfig = {
     "wait": 10
 }
 
-VERSION = "2.0.0-dev27-20210614"
+VERSION = "2.0.0-dev28-20210614"
 
-class Colours():
-    def __init__(self: object, warning: int, reset: int) -> NoReturn:
-        self.WARNING = fg(warning)
-        self.RESET = attr(reset)
-
-class Globals():
+"""
+    Globals is the miscellaneous global class, containing
+    essential variables that don't belong in any of the
+    other 3. Currently, that means the config settings and
+    the version number.
+"""
+class Globals:
+    
     def __init__(self: object, VERSION: int) -> NoReturn:
         self.config = {}
-        self.ConsoleColours = Colours(130, 0)
-        self.HOME = expanduser("~")
-        self.log = []
-        self.SAVE_PATH = defineSavePath(self.HOME)
-        self.Stats = Statistics()
         self.VERSION = VERSION
-
-class Statistics():
+    
+    def editConfig(self: object, key: str, value: Any) -> NoReturn:
+        self.config[key] = value
+   
+"""
+    Log handles the logging system, surprise surprise. In
+    addition to containing the main log array, Log contains
+    data relating to the colouring of log output, and also
+    defines a simple API for access and modification to/of
+    the data contained therein.
+"""
+class Log:
+    
     def __init__(self: object) -> NoReturn:
-        self.data = {
+        self.ConsoleColours = self.Colours(130, 0)
+        self.__log = []
+    
+    class Colours:
+        
+        def __init__(self: object, warning: int, reset: int) -> NoReturn:
+            self.RESET = attr(reset)
+            self.WARNING = fg(warning)
+        
+    def clear(self: object, mode="all") -> NoReturn:
+        if mode == "all":
+            del self.__log[:]
+        elif mode == "recent":
+            del self.__log[len(self.__log)-1]
+        else:
+            print(self.warning("WARNING: Log.clear() received unknown mode '{mode}'."))
+    
+    def get(self: object, mode="all") -> Union[List, str, None]:
+        if mode == "all":
+            return self.__log
+        elif mode == "recent":
+            return self.__log[len(self.__log)-1]
+        else:
+            print(self.warning("WARNING: Log.get() received unknown mode '{mode}'."))
+            return None
+    
+    # Finds the current time and returns it in a human readable format.
+    def getTime(self: object, timeToFind: int) -> str:
+        return datetime.fromtimestamp(timeToFind).strftime("%Y-%m-%d %H:%M:%S")
+    
+    # Updates the log array and prints to console
+    def new(self: object, messages: List) -> bool:
+        
+        for message in messages:
+            
+            # Allows for dynamic passing of lists
+            if not message: continue
+            
+            currentTime = self.getTime(time())
+            
+            self.__log.append(f"{currentTime} - {message}\n")
+            print(f"{currentTime} - {message}") if Globals.config["printLogs"] else None
+        
+        return True
+    
+    def warning(self: object, message: str) -> str:
+        return self.ConsoleColours.WARNING + message + self.ConsoleColours.RESET
+
+"""
+    Statistics stores the current and total statistics
+    and provides a simple API for accessing and modifying
+    the data contained therein.
+"""
+class Statistics:
+    
+    def __init__(self: object) -> NoReturn:
+        self.__data = {
             "current": {
                 "counted": 0,
                 "deleted": 0,
@@ -93,22 +159,83 @@ class Statistics():
         }
         self.failed = False
     
-    def generateNewTotals(self: object) -> NoReturn:
-        self.data["total"] = {
-                    "counted": 0,
-                    "deleted": 0
+    def get(self: object, dataset: str, stat="") -> int:
+        if not stat:
+            return self.__data[dataset]
+        else:
+            return self.__data[dataset][stat]
+    
+    def generateNew(self) -> NoReturn:
+        self.__data["total"] = {
+                "counted": 0,
+                "deleted": 0
             }
     
-    def resetCurrent(self: object) -> NoReturn:
-        self.data["current"] = {
+    def increment(self: object, stat: str) -> NoReturn:
+        self.__data["current"][stat] += 1
+    
+    def reset(self: object) -> NoReturn:
+        self.__data["current"] = {
                 "counted": 0,
                 "deleted": 0,
                 "waitingFor": 0
             }
     
+    def setTotals(self: object, totals: Dict) -> NoReturn:
+        self.__data["total"] = totals
+    
     def updateTotals(self: object) -> NoReturn:
         for statistic in ["counted","deleted"]:
-            self.data["total"][statistic] += self.data["current"][statistic]
+            self.__data["total"][statistic] += self.__data["current"][statistic]
 
-def initialiseGlobals(VERSION: int) -> object:
-    return Globals(VERSION)
+"""
+    System class handles the most important variables to
+    do with file-handling. The paths to the data and config
+    directories are contained here, as well as the location
+    of the home directory and the auto-detected OS.
+"""
+class System:
+    
+    def __init__(self: object) -> NoReturn:
+        self.HOME = expanduser("~")
+        self.OS = platform
+        self.PATHS = self.definePaths(self.HOME, self.OS)
+    
+    # Defines save paths for config and data based on the user's OS    
+    def definePaths(self: object, home: str, os: str) -> List:
+        
+        # Gets first 3 characters of OS
+        os = ''.join(list(os)[:3])
+        
+        if os in ["dar", "lin", "win"]:
+            
+            # Windows is fucking stupid why would you use backslashes
+            paths = {
+                "config": environ["APPDATA"] + "\\oscr",
+                "data": environ["APPDATA"] + "\\oscr\data"
+            } if os == "win" else {
+                "config": home + "/.config/oscr",
+                "data": home + "/.oscr/data"
+            }
+                
+            #Create any missing paths/directories
+            for path in paths:
+                if not isdir(paths[path]):
+                    Log.new(f"Making path: {paths[path]}")
+                    for directory in paths[path].split("/")[1:]:
+                       if not isdir(paths[path].split(directory)[0] + directory):
+                           Log.new(f"Making directory: {paths[path].split(directory)[0]}{directory}")
+                           mkdir(paths[path].split(directory)[0] + directory)
+            return paths
+        
+        # Exit is OS is unsupported
+        else:
+            Log.new(Log.warning(f"Unsupported operating system: {os}, exiting."))
+            exit()
+      
+
+# Declare global classes
+Globals = Globals(VERSION)
+Log = Log()
+Stats = Statistics()
+System = System()
