f0d5c2fa171cc811ef5b0e9d2f3d9ca54b193828
Murdo Maclachlan <murdo@maclachlans.org.uk>
2021-06-14 21:43:46 +0100

    Refactor to OOP

--- core/log.py
+++ core/log.py
@@ -17,91 +17,61 @@
     Contact me at murdo@maclachlans.org.uk
 """
 
-import time
 import sys
-from os import mkdir
-from os.path import isdir
+from time import time
 from typing import List, NoReturn
-from .misc import getTime
+from .globals import Globals, Log, System
+global Globals, Log, System
 
 """
     This module contains functions relating to the handling
-    of log output within OSCR, from printing to console to
-    saving that output to a .txt file in ~/.oscr/data.
+    of log output to a file within OSCR.
     
     As with all other modules, the functions are listed in
     alphabetical order.
 """
 
-# Attempts to update log.txt, creating any missing files/directories.
-def attemptLog(Globals: object) -> bool:
-    
-    try: return writeLog(Globals)
-        
-    # Creates log.txt and/or the data directory, if necessary.
-    except FileNotFoundError:
-        doLog("No log.txt found; attempting to create.", Globals)
-        if not isdir(Globals.HOME+"/.oscr/data"):
-            mkdir(Globals.HOME+"/.oscr/data")
-        return writeLog(Globals)
-
-# Updates the log array and prints to console
-def doLog(messages: List, Globals: object) -> bool:
-    
-    for message in messages:
-        
-        # Allows for dynamic passing of lists
-        if not message: continue
-        
-        currentTime = getTime(time.time())
-        
-        try:
-            Globals.log.append(f"{currentTime} - {message}\n")
-            print(f"{currentTime} - {message}") if Globals.config["printLogs"] else None
-        except AttributeError as e:
-            print(warn(f"{currentTime} - Failed to add message to log; message is {messages}.", Globals))
-            print(warn(f"Error is: {e}", Globals))
-            return False
-    
-    return True
-
 # Exits OSCR while updating the log with some last messages
-def exitWithLog(messages: List, Globals: object) -> NoReturn:
-    doLog(messages, Globals)
-    if not Globals.config["logUpdates"] or not updateLog(["Exiting..."], Globals):
-        print("Exiting...")
+def exitWithLog(messages: List) -> NoReturn:
+    
+    Log.new(messages)
+    
+    updateLog(["Exiting..."]) if Globals.config["logUpdates"] else print("Exiting...")
+        
     sys.exit(0)
 
 # Updates the log file with the current log.
-def updateLog(messages: List, Globals: object) -> bool:
+def updateLog(messages: List) -> bool:
     
     # This check is necessary to avoid empty lines in log.txt and the console output,
     # as in some places in the program, updateLog() is called with an empty array to
     # prompt the program to update the file without adding any new lines.
-    if messages: doLog(messages, Globals)
+    if messages: Log.new(messages)
         
     if Globals.config["logUpdates"]:
-        if attemptLog(Globals):
-            del Globals.log[:]
+        
+        if writeLog():
+            Log.clear()
             return True
+        
         else:
-            print(warn(f"{getTime(time.time())} - Log error; disabling log updates for this instance."), Globals)
+            print(
+                Log.warning(f"WARNING: Error updating log; disabling log updates for this instance."),
+                Log.warning(f"Most recent log was:\n"),
+                Log.get(mode="recent")
+            )
             Globals.config["logUpdates"] = False
     
     return False
 
-# Colours warnings orange so that they stand out
-def warn(message: str, Globals: object) -> str:
-    return Globals.ConsoleColours.WARNING + message + Globals.ConsoleColours.RESET
-
 # Writes the contents of the log array to the log.txt file
-def writeLog(Globals: object) -> bool:
+def writeLog() -> bool:
     
     try:
-        with open(Globals.HOME+"/.oscr/data/log.txt", "a") as file:
-            for i in Globals.log: file.write(i)
+        with open(f"{System.PATHS['data']}/log.txt", "a") as file:
+            for i in Log.get("all"): file.write(i)
         return True
     
     # Catch all exceptions to avoid the program crashing;
     # updateLog will disable further log updates if it receives False.
-    except Exception: return False
+    except Exception as e: print(e); return False
