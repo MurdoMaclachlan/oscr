f43d01a050688909239885a77270b1e42a00a032
Murdo Maclachlan <47920497+MurdoMaclachlan@users.noreply.github.com>
2021-10-16 19:17:25 +0100

    Optimise login(), add init() for main.py optimisation

--- oscrcore/auth.py
+++ oscrcore/auth.py
@@ -23,95 +23,112 @@
 
 import praw
 import random
-import webbrowser
 import sys
 import socket
+import webbrowser
+from configparser import NoSectionError
+from praw.exceptions import MissingRequiredAttributeException
 from typing import NoReturn
 from .globals import Globals, Log, System
-from .ini import addRefreshToken, getCredentials
+from .ini import addRefreshToken, createIni, getCredentials
+from .log import exitWithLog
 global Globals, Log, System
+
+
+def checkFailure(client: object, params: Dict, state: str) -> NoReturn:
+    if state != params['state']:
+        sendMessage(client, f'State mismatch. Expected: {state} Received: {params["state"]}')
+        Log.new([f'State mismatch. Expected: {state} Received: {params["state"]}'])
+        sys.exit()
+    elif 'error' in params:
+        sendMessage(client, params['error'])
+        Log.new([params['error']])
+        sys.exit()
+
+
+def init() -> object:
+    try:
+        return login()
+
+    # Catch for invalid praw.ini, will create a new one then restart the program;
+    # the restart is required due to current PRAW limitations. :'(
+    except (NoSectionError, MissingRequiredAttributeException, KeyError):
+        exitWithLog(
+            ["praw.ini successfully created, program restart required for this to take effect."]
+        ) if createIni() else exitWithLog(
+            [Log.warning("WARNING: Failed to create praw.ini file, something went wrong.")]
+        )
 
 
 # I don't know how half of this shit works but it does work so I don't care
 def login() -> object:
     
-    credentials = getCredentials()
-    
+    creds = getCredentials()
+
+    constants = {
+        "user_agent": f"{System.OS}:oscr:v{Globals.VERSION}(by /u/MurdoMaclachlan)",
+        "client_id": creds["client_id"],
+        "client_secret": creds["client_secret"],
+        "username": creds["username"],
+    }
+
     if Globals.config["useRefreshTokens"]:
-        
+
         # Indicates user will not have authorised OSCR yet
-        if "refresh_token" not in credentials.keys():
-            
+        if "refresh_token" not in creds.keys():
             reddit = praw.Reddit(
-                user_agent = f"{System.OS}:oscr:v{Globals.VERSION}(by /u/MurdoMaclachlan)",
-                client_id = credentials["client_id"],
-                client_secret = credentials["client_secret"],
-                username = credentials["username"],
-                password = credentials["password"],
-                redirect_uri = credentials["redirect_uri"]
+                password = creds["password"],
+                redirect_uri = creds["redirect_uri"],
+                **constants
             )
-        
-            try:
-                reddit.user.me()
-            
+
+            try: reddit.user.me()
+
             # If user has 2FA enabled but has not authorised OSCR
             except Exception as e:
                 if (str(e) != 'invalid_grant error processing request'):
                     Log.new(Log.warning([f"LOGIN FAILURE, ERROR IS: {e}"]))
                     sys.exit(0)
+
+                # ngl idk wtf this does but it sure does do
                 else:
                     state = str(random.randint(0, 65000))
                     scopes = ['history', 'read', 'edit']
                     url = reddit.auth.url(scopes, state, 'permanent')
-                    Log.new(["2FA enabled, but not authorised. OSCR will now open a window in your browser to complete the login process."])
+                    Log.new(["2FA enabled, but not authorised. OSCR will now open a tab in your browser to complete the login process."])
                     webbrowser.open(url)
-        
+
                     client = receiveConnection()
                     data = client.recv(1024).decode('utf-8')
                     paramTokens = data.split(' ', 2)[1].split('?', 1)[1].split('&')
                     params = {key: value for (key, value) in [token.split('=') for token in paramTokens]}
-        
-                    if state != params['state']:
-                        sendMessage(client, f'State mismatch. Expected: {state} Received: {params["state"]}')
-                        Log.new([f'State mismatch. Expected: {state} Received: {params["state"]}'])
-                        sys.exit()
-                    elif 'error' in params:
-                        sendMessage(client, params['error'])
-                        Log.new([params['error']])
-                        sys.exit()
-        
+
+                    # Check for an authorisation failure
+                    checkFailure(client, params, state)
+
                     refreshToken = reddit.auth.authorize(params["code"])
-                    
                     addRefreshToken(refreshToken)
                     sendMessage(
                         client,
                         f"Refresh token: {refreshToken}. Feel free to close this page. This message is simply for success confirmation; " +
                         "it is not necessary to save your refresh_token, as OSCR has automatically done this."
                     )
-             
+
         # If user has 2FA enabled and has already authorised OSCR
         else:
             reddit = praw.Reddit(
-                user_agent = f"{System.OS}:oscr:v{Globals.VERSION}(by /u/MurdoMaclachlan)",
-                client_id = credentials["client_id"],
-                client_secret = credentials["client_secret"],
-                username = credentials["username"],
-                refresh_token = credentials["refresh_token"]
+                refresh_token = creds["refresh_token"],
+                **constants
             )
-   
+
     # If user does not have 2FA enabled
     else:
         reddit = praw.Reddit(
-            user_agent = f"{System.OS}:oscr:v{Globals.VERSION}(by /u/MurdoMaclachlan)",
-            client_id = credentials["client_id"],
-            client_secret = credentials["client_secret"],
-            username = credentials["username"],
-            password = credentials["password"]
+            password = creds["password"],
+            **constants
         )
-        
-        try:
-            reddit.user.me()
-        
+
+        try: reddit.user.me()
         except Exception as e:
             if (str(e) != 'invalid_grant error processing request'):
                 Log.new(Log.warning([f"LOGIN FAILURE, ERROR IS: {e}"]))
@@ -119,7 +136,7 @@ def login() -> object:
             else:
                 Log.new([Log.warning("2FA detected, but not enabled. Please run 'oscr -S' and set useRefreshTokens to True, then re-run OSCR.")])
                 sys.exit(0)
-    
+
     return reddit
 
 
