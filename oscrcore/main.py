965086f3787af51d03cb20af13c8c69d8ee99a3c
Murdo Maclachlan <47920497+MurdoMaclachlan@users.noreply.github.com>
2021-10-23 13:26:39 +0100

    update contact e-mail

--- oscrcore/main.py
+++ oscrcore/main.py
@@ -14,7 +14,7 @@
     You should have received a copy of the GNU General Public License
     along with this program. If not, see <https://www.gnu.org/licenses/>.
     
-    Contact me at murdo@maclachlans.org.uk
+    Contact me at murdomaclachlan@duck.com
 """
 
 import praw
